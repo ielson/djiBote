efb0460dc6459ce9334034218dbd6ca2309191b1
ielson <danmascandrade@gmail.com>
2020-01-14 15:56:53 -0300

    Changed package name and identifier

---
+++ DJIBote/app/src/main/java/com/ielson/djiBote/MainActivity.java
@@ -0,0 +1,133 @@
+package com.ielson.djiBote;
+
+import android.Manifest;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
+import dji.sdk.base.BaseComponent;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
+    private static BaseProduct mProduct;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // When the compile and target version is higher than 22, please request the following permission at runtime to ensure the SDK works well.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
+                            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
+                            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
+                            Manifest.permission.READ_PHONE_STATE,
+                    }
+                    , 1);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        //Initialize DJI SDK Manager
+        mHandler = new Handler(Looper.getMainLooper());
+        DJISDKManager.getInstance().registerApp(this, mDJISDKManagerCallback);
+    }
+
+    private DJISDKManager.SDKManagerCallback mDJISDKManagerCallback = new DJISDKManager.SDKManagerCallback() {
+
+        @Override
+        public void onRegister(DJIError error) {
+            Log.d(TAG, error == null ? "success" : error.getDescription());
+            if(error == DJISDKError.REGISTRATION_SUCCESS) {
+                DJISDKManager.getInstance().startConnectionToProduct();
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_LONG).show();
+                    }
+                });
+            } else {
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "register sdk failed, check if network is available", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+            Log.e("TAG", error.toString());
+        }
+
+        @Override
+        public void onProductChange(BaseProduct oldProduct, BaseProduct newProduct) {
+
+            mProduct = newProduct;
+            if(mProduct != null) {
+                mProduct.setBaseProductListener(mDJIBaseProductListener);
+            }
+
+            notifyStatusChange();
+        }
+    };
+
+    private BaseProduct.BaseProductListener mDJIBaseProductListener = new BaseProduct.BaseProductListener() {
+
+        @Override
+        public void onComponentChange(BaseProduct.ComponentKey key, BaseComponent oldComponent, BaseComponent newComponent) {
+            if(newComponent != null) {
+                newComponent.setComponentListener(mDJIComponentListener);
+            }
+            notifyStatusChange();
+        }
+
+        @Override
+        public void onConnectivityChange(boolean isConnected) {
+            notifyStatusChange();
+        }
+
+    };
+
+    private BaseComponent.ComponentListener mDJIComponentListener = new BaseComponent.ComponentListener() {
+
+        @Override
+        public void onConnectivityChange(boolean isConnected) {
+            notifyStatusChange();
+        }
+
+    };
+
+    private void notifyStatusChange() {
+        mHandler.removeCallbacks(updateRunnable);
+        mHandler.postDelayed(updateRunnable, 500);
+    }
+
+    private Runnable updateRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
+            sendBroadcast(intent);
+        }
+    };
+
+}
